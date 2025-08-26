4dabf54b2e29c9bb465493c5eee3ca2531fcfbd7
nichphu2k2 <nichphu2k2@gmail.com>
2025-08-27 02:20:50 +0700

    Create 2.install_docker.sh

---
+++ 2.install_docker.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo systemctl restart docker
+sudo usermod -aG docker ${USER}
+sudo apt install docker-compose -y
+su - ${USER}
