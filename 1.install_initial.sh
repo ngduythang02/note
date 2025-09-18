a9ad27043414f7b3904bb584ed68ba674a5c0ec0
ngduythang02 <149552584+ngduythang02@users.noreply.github.com>
2025-09-18 20:47:43 +0700

    Update 1.install_initial.sh

--- 1.install_initial.sh
+++ 1.install_initial.sh
@@ -4,3 +4,5 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install net-tools -y
 sudo apt install vim -y
+echo 'export HISTTIMEFORMAT="%F %T "' | sudo tee -a /etc/bash.bashrc
+source /etc/bash.bashrc
