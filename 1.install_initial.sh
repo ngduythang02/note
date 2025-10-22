e6c7f7c3cb72af7d5748d8278d396311ddf47b26
ngduythang02 <149552584+ngduythang02@users.noreply.github.com>
2025-10-22 20:08:52 +0700

    Update 1.install_initial.sh

--- 1.install_initial.sh
+++ 1.install_initial.sh
@@ -3,6 +3,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install net-tools -y
+sudo timedatectl set-timezone Asia/Ho_Chi_Minh
 sudo apt install vim -y
 echo 'export HISTTIMEFORMAT="%F %T "' | sudo tee -a /etc/bash.bashrc
 source /etc/bash.bashrc
