21d1b2209582d4277279c06b3a9f6f43ec5b3e9d
nichphu2k2 <nichphu2k2@gmail.com>
2026-02-05 14:47:27 +0700

    Create 10.install_mailcow.sh

---
+++ 10.install_mailcow.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ "$(whoami)" = "root" ]; then
+    echo "You need to run this script with USER NORMAL"
+    exit 1
+fi
+
+sudo apt update
+sudo apt install -y jq curl git ca-certificates gnupg lsb-release
+cd
+git clone https://github.com/mailcow/mailcow-dockerized
+cd mailcow-dockerized
+sudo apt install -y jq curl git ca-certificates gnupg lsb-release
+./generate_config.sh
+
+read -rp "Start 'docker compose pull'? (Y/n): " answer
+case "$answer" in
+  [Yy]|"")
+    echo "→ Running docker compose pull..."
+    docker compose pull
+    ;;
+  [Nn])
+    echo "→ The script has stopped as requested."
+    exit 0
+    ;;
+  *)
+    echo "→ Invalid selection. Stop script."
+    exit 1
+    ;;
+esac
+
+read -rp "Start 'docker compose pull'? (Y/n): " answer
+case "$answer" in
+  [Yy]|"")
+    echo "→ Running docker compose pull..."
+    docker compose up -d
+    ;;
+  [Nn])
+    echo "→ The script has stopped as requested."
+    exit 0
+    ;;
+  *)
+    echo "→ Invalid selection. Stop script."
+    exit 1
+    ;;
+esac
+
