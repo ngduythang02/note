2dc3b775fa827704800543ed790419de451e9536
nichphu2k2 <nichphu2k2@gmail.com>
2026-04-29 17:19:01 +0700

    Create 11.increase_swap.sh

---
+++ 11.increase_swap.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "$(whoami)" != "root" ]; then
+    echo "You need to run this script with root privileges"
+    exit 1
+fi
+
+read -p "Enter the required additional SWAP capacity (Gb) (EX: 4): " swap
+
+swap_old="0G"
+swap_old=$(swapon --show | awk 'NR==2 {print $3}')
+swapoff -a
+rm -f /swap.img
+fallocate -l ${swap}G /swap.img
+chmod 600 /swap.img
+mkswap /swap.img
+swapon /swap.img
+sed -i '/swap.img/d' /etc/fstab
+echo '/swap.img none swap sw 0 0' >> /etc/fstab
+swap_new=$(swapon --show | awk 'NR==2 {print $3}')
+
+echo ""
+echo ""
+echo ""
+free -h
+echo ""
+echo ""
+echo ""
+echo ""
+echo "✅ SWAP upgrade from ${swap_old} to ${swap_new} "
+echo ""
+echo ""
+echo ""
