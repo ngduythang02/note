a300135ff9c5ded65c19a49d31f64aadaecea919
nichphu2k2 <nichphu2k2@gmail.com>
2026-05-06 13:44:20 +0700

    Update 12.install_wireguard_server.sh

--- 12.install_wireguard_server.sh
+++ 12.install_wireguard_server.sh
@@ -5,18 +5,18 @@ if [ "$(whoami)" != "root" ]; then
     exit 1
 fi
 # Variable
-SERVER_PRIVATE_KEY=$(tr -d '\n' < /tools/wireguard/server_private.key)
 NIC=$(ip route get 8.8.8.8 | awk '{print $5; exit}')
 read -p "Enter Subnet Private (Recomment: 192.168.1.0/24): " SUBNET_PRIVATE
 
 #Install-wireguard-server
 apt update -y
 apt install wireguard -y
-mkdir /tools/wireguard/
+mkdir -p /tools/wireguard/
 chown -R root:root /tools/wireguard
 cd /tools/wireguard/
 wg genkey | tee server_private.key | wg pubkey > server_public.key
 wg genkey | tee client_private.key | wg pubkey > client_public.key
+SERVER_PRIVATE_KEY=$(tr -d '\n' < /tools/wireguard/server_private.key)
 
 tee /etc/wireguard/wg0.conf > /dev/null <<EOF
 [Interface]
@@ -28,9 +28,6 @@ ListenPort = 51820
 PostUp = iptables -t nat -A POSTROUTING -o ${NIC} -j MASQUERADE
 PostDown = iptables -t nat -D POSTROUTING -o ${NIC} -j MASQUERADE
 
-[Peer]
-PublicKey = <client_public_key_windowns>
-AllowedIPs = 14.14.14.2/32
 EOF
 echo "net.ipv4.ip_forward=1" > /etc/sysctl.d/99-wireguard.conf
 sysctl --system
@@ -40,8 +37,16 @@ systemctl enable wg-quick@wg0
 iptables -A FORWARD -i wg0 -o ${NIC} -j ACCEPT
 iptables -A FORWARD -i ${NIC} -o wg0 -j ACCEPT
 iptables -t nat -A POSTROUTING -s 14.14.14.0/24 -d ${SUBNET_PRIVATE} -j MASQUERADE
-echo -e "\n\n\n"
+echo "********************"
+echo ""
+echo ""
+echo ""
 wg show
-echo -e "\n\n\n"
-echo -e "INSTALL WireGuard SUCCESSFULL!"
-echo -e "\n\n\n"
+echo ""
+echo ""
+echo ""
+echo "INSTALL WireGuard SUCCESSFULL!"
+echo ""
+echo ""
+echo ""
+echo "********************"
