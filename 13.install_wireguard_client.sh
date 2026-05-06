f2fa0b0794fffd71a9fbbf20a66daa08fd3ecb65
nichphu2k2 <nichphu2k2@gmail.com>
2026-05-06 13:45:12 +0700

    Update 13.install_wireguard_client.sh

--- 13.install_wireguard_client.sh
+++ 13.install_wireguard_client.sh
@@ -10,10 +10,14 @@ IP_PUBLIC=$(curl -s https://api.ipify.org || curl -s https://ifconfig.me)
 read -p "Name's client (Recomment: DNG-TECHNICAL): " name
 read -p "Input windows client public key (Recomment: aNw9opON6ARPD0wN3tzcgLxO+OiV9Ybb0l4hE8CKsmY=): " CLIENT_PUBLIC_KEY
 read -p "Enter Subnet Private (Recomment: 192.168.1.0/24): " SUBNET_PRIVATE
-
-echo -e "IP VPN already used:"
+echo ""
+echo ""
+echo ""
+echo "IP VPN already used:"
 grep -oE '14\.14\.14\.[0-9]+' /etc/wireguard/wg0.conf
-echo -e "\n\n\n"
+echo ""
+echo ""
+echo ""
 read -p "IP VPN new (Recomment: 14.14.14.x): " IP_VPN_NEW
 
 #Install-wireguard-client
@@ -27,10 +31,19 @@ EOF
 wg-quick down wg0
 wg-quick up wg0
 
-echo -e "\n\n\n"
+echo "********************"
+echo ""
+echo ""
+echo ""
 wg show
-echo -e "\n\n\n"
-cat > client.conf <<EOF
+echo ""
+echo ""
+echo ""
+echo "********************"
+echo ""
+echo ""
+echo ""
+cat > /tools/wireguard/client.conf <<EOF
 [Interface]
 PrivateKey = <private_key_windows>
 Address = ${IP_VPN_NEW}/24
@@ -42,6 +55,17 @@ AllowedIPs = 14.14.14.0/24, ${SUBNET_PRIVATE}
 Endpoint = ${IP_PUBLIC}:51820
 PersistentKeepalive = 25
 EOF
-echo -e "\n\n\n"
-echo -e "https://drive.google.com/file/d/1wZOKzRix4edICuLQqaynDc5KD1G2L40M/view?usp=sharing"
-echo -e "Access the link to download the Windows client WireGuard."
+echo "Paste the following content into the Windows client WireGuard."
+cat /tools/wireguard/client.conf
+echo ""
+echo ""
+echo ""
+echo "********************"
+echo ""
+echo ""
+echo ""
+echo "https://drive.google.com/file/d/1wZOKzRix4edICuLQqaynDc5KD1G2L40M/view?usp=sharing"
+echo "Access the link to download the Windows client WireGuard."
+echo ""
+echo ""
+echo ""
